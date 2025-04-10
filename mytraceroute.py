9409144021f553290cc1f4b1a0a3787e741b08f9
one2th <patrychenku99@gmail.com>
2025-04-10 11:52:50 +0300

    Update mytraceroute.py

--- mytraceroute.py
+++ mytraceroute.py
@@ -1,11 +1,9 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
 import socket as sckt
 import os
 import struct
-import time
 import time as t
-import threading
 import ipaddress
 import sys
 
@@ -19,13 +17,9 @@ try:
 except ValueError:
     print('Неверный IP-адрес')
     exit()
-id = os.getpid() & 0xFFFF
-is_interrupt = False
-dst = sys.argv[1]
-timeout_index = 0
-hops_num = -1
-packet_ind = 0
 
+id = os.getpid() & 0xFFFF
+dst = sys.argv[1]
 
 def get_icmp_8(seq):
     type = 8
@@ -39,91 +33,118 @@ def get_icmp_8(seq):
     packet = struct.pack('!BBHHH', type, code, checksum, id, seq) + data
     return packet
 
-is_in_route = False
 sending_times = [[-1, -1, -1] for i in range(0, 30)]
 delays = [[-1, -1, -1] for i in range(0, 30)]
 src_ips = [['', '', ''] for i in range(0, 30)]
 
-def send_requests():
-    socket = sckt.socket(sckt.AF_INET, sckt.SOCK_RAW, sckt.IPPROTO_ICMP)
-    global is_in_route
-    is_in_route = True
-    i=0
-    while is_in_route and i<30:
-        ttl = i+1
-        for j in range(1, 4):
-            socket.setsockopt(sckt.IPPROTO_IP, sckt.IP_TTL, ttl)
-            sending_times[i][j - 1] = t.time()
-            socket.sendto(get_icmp_8(i*3+j), (dst, 33434))
-        i+=1
 
-def receive_replies():
-    socket = sckt.socket(sckt.AF_INET, sckt.SOCK_RAW, sckt.IPPROTO_ICMP)
-    socket.bind(('', 0))
-    socket.setsockopt(sckt.SOL_SOCKET, sckt.SO_RCVBUF, 65535)
-    socket.settimeout(2)
-    global delays, is_in_route, hops_num
-    while not is_in_route:
-        pass
-    while is_in_route:
+print_seq = 0
+def print_trace(end):
+    global print_seq
+    while print_seq <= end:
+        i = print_seq // 3
+        j = print_seq % 3
+        if j == 0:
+            print(f'\n{i+1} ', end='')
+            if delays[i][j] == -1:
+                print('*', end='')
+            else:
+                print(f'({src_ips[i][j]}) {delays[i][j]:.3f} ms', end='')
+        elif delays[i][j] == -1:
+            print(' *', end='')
+        elif src_ips[i][j] == src_ips[i][j-1]:
+            print(f' {delays[i][j]:.3f} ms', end='')
+        else:
+            print(f' ({src_ips[i][j]}) {delays[i][j]:.3f} ms', end='')
+        print_seq += 1
+
+print(f'traceroute to {dst} 30 hops max, 16 bytes packets', end='', flush=True)
+socket = sckt.socket(sckt.AF_INET, sckt.SOCK_RAW, sckt.IPPROTO_ICMP)
+socket.bind(('', 0))
+socket.settimeout(2)
+i=0
+ttl=0
+send_seq=0
+for i in range(0, 16):
+    send_seq += 1
+    ttl = ((send_seq-1)//3) + 1
+    socket.setsockopt(sckt.IPPROTO_IP, sckt.IP_TTL, ttl)
+    sending_times[ttl-1][(send_seq - 1) % 3] = t.time()
+    socket.sendto(get_icmp_8(send_seq), (dst, 0))
+
+in_route = True
+
+while in_route:
+    count = 0
+    rec_seq = 0
+    while True:
         try:
-            packet, addr= socket.recvfrom(1024)
-            icmp_type = int(packet[20])
+            packet, addr = socket.recvfrom(1024)
+            time_ = t.time()
+            count += 1
         except TimeoutError:
             break
-        if len(packet) >= 56:
+        icmp_type = packet[20]
+        if icmp_type == 11:
             pid = (packet[52] << 8) + packet[53]
-            if pid == id:
-                if icmp_type == 11:
-                    seq = int((packet[54] << 8) + packet[55]) - 1
-                    delays[seq//3][seq % 3] = t.time() - sending_times[seq//3][seq%3]
-                    src_ips[seq // 3][seq % 3]=addr[0]
+            if pid!=id:
+                continue
+            rec_seq = (packet[54] << 8) + packet[55] - 1
+            i = rec_seq // 3
+            j = rec_seq % 3
+            delays[i][j] = (time_ - sending_times[i][j]) * 1000
+            src_ips[i][j] = addr[0]
+            if rec_seq + 1 == send_seq:
+                break
         if icmp_type == 0:
             if addr[0] == dst:
-                seq = int((packet[26] << 8) + packet[27]) - 1
-                delays[seq // 3][seq % 3] = t.time() - sending_times[seq // 3][seq % 3]
-                src_ips[seq // 3][seq % 3] = addr[0]
-                hops_num = (seq // 3) + 1
-                count = 1
-                while count<3 and is_in_route:
+                rec_seq = int((packet[26] << 8) + packet[27]) - 1
+                delays[rec_seq // 3][rec_seq % 3] = (t.time() - sending_times[rec_seq // 3][rec_seq % 3]) * 1000
+                src_ips[rec_seq // 3][rec_seq % 3] = addr[0]
+                for i in range(0, 3-(send_seq - rec_seq)):
+                    send_seq += 1
+                    ttl = ((send_seq - 1) // 3) + 1
+                    socket.setsockopt(sckt.IPPROTO_IP, sckt.IP_TTL, ttl)
+                    sending_times[ttl - 1][(send_seq - 1) % 3] = t.time()
+                    socket.sendto(get_icmp_8(send_seq), (dst, 0))
+                count_repl = 1
+                while count_repl < 3:
                     try:
                         packet, addr = socket.recvfrom(1024)
+                        time_ = t.time()
                     except TimeoutError:
+                        in_route = False
                         break
-                    if addr[0] == dst:
-                        seq = int((packet[26] << 8) + packet[27]) - 1
-                        delays[seq // 3][seq % 3] = t.time() - sending_times[seq // 3][seq % 3]
-                        src_ips[seq // 3][seq % 3] = addr[0]
-                        count += 1
-                is_in_route = False
+                    icmp_type = packet[20]
+                    if icmp_type == 0:
+                        rec_seq = int((packet[26] << 8) + packet[27]) - 1
+                        delays[rec_seq // 3][rec_seq % 3] =  (
+                                    t.time() - sending_times[rec_seq // 3][rec_seq % 3]) * 1000
+                        src_ips[rec_seq // 3][rec_seq % 3] = addr[0]
+                        count_repl+=1
+                    elif icmp_type == 11:
+                        pid = (packet[52] << 8) + packet[53]
+                        if pid != id:
+                            continue
+                        rec_seq = (packet[54] << 8) + packet[55] - 1
+                        i = rec_seq // 3
+                        j = rec_seq % 3
+                        delays[i][j] = (time_ - sending_times[i][j]) * 1000
+                        src_ips[i][j] = addr[0]
+                in_route = False
+                break
 
-receiving_thread = threading.Thread(target=receive_replies)
-receiving_thread.start()
+    print_trace(rec_seq)
 
-send_requests()
+    if not in_route:
+        print_trace(rec_seq)
+        for i in range(0, (2 - rec_seq % 3)):
+            print(' *', end="")
+        break
 
-time.sleep(0.001)
-while is_in_route and time.time()-sending_times[29][2]<2:
-    pass
-
-print(f'traceroute to {dst}, 30 hops max, 34 bytes packets')
-for i in range(0, hops_num if hops_num!=-1 else 30):
-    print(f'{i+1} ', end='')
-    if delays[i][0]==-1:
-        print('* ', end='')
-    else:
-        print(f'({src_ips[i][0]}) {delays[i][0]*1000:.3f}ms ', end='')
-    if delays[i][1]==-1:
-        print('* ', end='')
-    elif src_ips[i][1]==src_ips[i][0]:
-        print(f'{delays[i][1]*1000:.3f}ms ', end='')
-    else:
-        print(f'({src_ips[i][1]}) {delays[i][0] * 1000:.3f}ms ', end='')
-    if delays[i][2]==-1:
-        print('* ')
-    elif src_ips[i][2]==src_ips[i][1]:
-        print(f'{delays[i][2]*1000:.3f} ms ')
-    else:
-        print(f'({src_ips[i][2]}){delays[i][2] * 1000:.3f}ms ')
-
-receiving_thread.join()
+    for i in range(0, count if count != 0 else 16):
+        send_seq += 1
+        ttl = ((send_seq - 1) // 3) + 1
+        socket.setsockopt(sckt.IPPROTO_IP, sckt.IP_TTL, ttl)
+        socket.sendto(get_icmp_8(send_seq), (dst, 33434))
+        sending_times[ttl - 1][(send_seq - 1) % 3] = t.time()
